c09335b64739883a81993cb7e2d3a0fddc22de87
xiafan-a <2735810459@qq.com>
2025-03-02 15:45:19 +0800

    3/2

--- src/main/java/com/dy/picture/PictureApplication.java
+++ src/main/java/com/dy/picture/PictureApplication.java
@@ -1,9 +1,15 @@
 package com.dy.picture;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("com.dy.picture.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
+
 public class PictureApplication {
 
     public static void main(String[] args) {
